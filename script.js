1e0248e84b4aeada3c7c020f6df8ad0a2ffde677
aleksJoker <aleks.valushko@yandex.ru>
2020-07-26 13:04:40 +0300

    functions added

--- script.js
+++ script.js
@@ -1,75 +1,68 @@
-/* Задание на урок:
+let numberOfFilms;
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
     }
+}
 
-Проверить, чтобы все работало без ошибок в консоли */
+start();
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
-    personalMovieDB = {
-        count: (numberOfFilms === '') ? 0 : numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
-    };
+let personalMovieDB = {
+    count: (numberOfFilms === '') ? 0 : numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
 
 let number = 0,
     movie,
     movieRating;
 
-while (number < 2) {
-    movie = prompt('Один из последних просмотренных фильмов?', '');
-    movieRating = prompt('На сколько оцените его?', '');
-    if ((movie || movieRating) === '' || (movie || movieRating) === null || (movie.length || movieRating.length) > 50) {
+function rememberFilms() {
+    while (number < 2) {
         movie = prompt('Один из последних просмотренных фильмов?', '');
         movieRating = prompt('На сколько оцените его?', '');
+        if ((movie || movieRating) === '' || (movie || movieRating) === null ||
+            (movie.length || movieRating.length) > 50) {
+            movie = prompt('Один из последних просмотренных фильмов?', '');
+            movieRating = prompt('На сколько оцените его?', '');
+        }
+        personalMovieDB.movies[movie] = movieRating;
+        number++;
     }
-    personalMovieDB.movies[movie] = movieRating;
-    number++;
 }
 
-if (personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert('Вы классический зритель');
-} else if (personalMovieDB.count > 30) {
-    alert('Вы киноман');
-} else {
-    alert('Произошла ошибка');
+rememberFilms();
+
+function personLevel() {
+    if (personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        alert('Вы классический зритель');
+    } else if (personalMovieDB.count > 30) {
+        alert('Вы киноман');
+    } else {
+        alert('Произошла ошибка');
+    }
 }
 
-console.log(personalMovieDB);
+personLevel();
 
-/* Задание на урок:
+function showMyDB() {
+    if (personalMovieDB.privat === false) {
+        console.log(personalMovieDB);
+    }
+}
 
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+showMyDB();
 
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
-// Код возьмите из предыдущего домашнего задания
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        personalMovieDB.genres.push(prompt(`Ваш любимый жанр под номером ${i}?`, ''));
+    }
+}
+writeYourGenres();
